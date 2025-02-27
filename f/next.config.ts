45058a2b88f71d2a480da66c4a875e03c6884710
dikanevn <dikanevn@user-pc.>
2025-02-27 12:24:39 +0300

    q

--- f/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  basePath: '/ynftnext',
-  assetPrefix: '/ynftnext/',
-  output: 'export',
-};
-
-export default nextConfig;
