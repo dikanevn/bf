8b2bc474092c2338c8cc7cdfa9fddcfc7816a2c0
dikanevn <dikanevn@user-pc.>
2025-03-02 00:27:52 +0300

    q

--- b/k01.ts
+++ b/k01.ts
@@ -17,7 +17,7 @@ async function main() {
   console.log("Начинаем поиск красивого адреса...");
   
   // Здесь можно изменить префикс, который вы хотите найти
-  const desiredPrefix = "YAP";
+  const desiredPrefix = "inf";
   const keypair = generateBeautifulAddress(desiredPrefix);
   
   const publicKey = keypair.publicKey.toString();
@@ -25,7 +25,7 @@ async function main() {
   
   // Сохраняем секретный ключ в файл
   const secretKeyArray = Array.from(keypair.secretKey);
-  const filename = `solana-key-${publicKey.slice(0, 8)}.json`;
+  const filename = `.env_key-${publicKey.slice(0, 8)}.json`;
   
   fs.writeFileSync(filename, JSON.stringify(secretKeyArray));
   console.log(`Секретный ключ сохранен в файл: ${filename}`);
